370602bb15aaa83d5a976ea255883a8c9d0b9152
Sandeep Koranne <sandeep.koranne+github@gmail.com>
2015-04-01 20:48:54 -0700

    Add hello world C example

---
+++ EXAMPLE_CODE/hw.c
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ *  File    : hw.c
+ *  Author  : Sandeep Koranne
+ *
+ * Purpose  : Hello, World in C
+ *
+ ******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  printf("Hello, World!\n");
+  return ( EXIT_SUCCESS );
+}
